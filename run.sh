dd0125b4f45bf01665fec7c2b2bf5038504597b0
Son Dieu <sdieu@Sons-MacBook-Pro.local>
2020-03-24 10:14:27 +0700

    update command

--- run.sh
+++ run.sh
@@ -2,29 +2,50 @@
 
 if [[ $@ ]];
     then
-    if [[ "$@" == "setup" ]]
+    if [[ "$@" == "cleanup" ]]
         then
-        rm -rf ./customform && rm -rf ./deploy && git clone  --single-branch git@github.com:Skedulo/phoenix_customforms.git ./customform && rm -rf ./customform/public
+        rm -rf ./customform && rm -rf ./deploy
+        exit 0
+    fi
+
+    if [[ "$@" == "download-form" ]]
+        then
+        node index.js
+        exit 0
+    fi
+
+    if [[ "$@" == "setup-form" ]]
+        then
+        git clone  --single-branch git@github.com:Skedulo/phoenix_customforms.git ./customform && rm -rf ./customform/public
         unzip  -P skedcoins ./download/viewSources.zip -d ./customform 
-        mkdir ./customform/deploy
         exit 0
     fi
 
     if [[ "$@" == "bootstrap" ]]
         then
-        ./customform/run.sh bootstrap
+        (cd customform && ./run.sh bootstrap)
         exit 0
     fi
 
     if [[ $1 == "compile" ]]
         then
-        ./customform/run.sh compile-deploy deploy_pkg
-        mkdir deploy && mv ./customform/deploy/deploy_pkg.tar.gz ./deploy
+        rm -rf ./customform/deploy && mkdir ./customform/deploy 
+        (cd customform && ./run.sh compile-deploy deploy_pkg)
+        mv ./customform/deploy/deploy_pkg.tar.gz ./deploy
+        exit 0
+    fi
+
+    if [[ "$@" == "dev-server" ]]
+        then
+        (cd customform && ./run.sh dev-server)
         exit 0
     fi
 
     else 
-        node index.js
+        ./run.sh cleanup
+        ./run.sh download-form
+        ./run.sh setup-form
+        ./run.sh bootstrap
         ./run.sh compile
         exit 0
 fi
